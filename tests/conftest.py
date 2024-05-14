5133c88c613adf7f2527aadd1c0b6a75cdad74f6
lightgleb <prorocker7@gmail.com>
2024-05-14 17:24:04 +0300

    Поправил конфтест

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,42 +12,34 @@ from online_market.utils import attach
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
+    browser.config.driver = driver
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
     browser.config.base_url = "https://www.online-market.by"
 
-    yield
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": "100.0",
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    # options.capabilities.update(selenoid_capabilities)
-    # driver = webdriver.Remote(
-    #     command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-    #     options=options
-    # )
-    #
-    # browser.config.driver = driver
-    # browser.config.window_height = 1920
-    # browser.config.window_width = 1080
-    # browser.config.base_url = "https://www.online-market.by"
-    #
-    # yield browser
-    #
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_html(browser)
-    # attach.add_video(browser)
-    #
-    # browser.quit()
 
 
 @pytest.fixture(scope='function')
